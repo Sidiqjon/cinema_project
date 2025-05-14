47c7a30c9b1ec899947475c7807a4c6b19d383ea
Sidiqjon <sidiqjonyusufjanov7@gmail.com>
2025-05-14 08:41:03 +0500

    Completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { NavLink } from "react-router-dom";  
 import logo from "@/assets/movietime.svg";
 import search from "@/assets/search.svg";
 import tv from "@/assets/tv.svg";
