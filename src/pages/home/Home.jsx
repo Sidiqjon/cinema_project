c348d53e484a379101e36fe10cec94291685b96c
Sidiqjon <sidiqjonyusufjanov7@gmail.com>
2025-05-15 08:31:39 +0500

    Developing

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import MovieView from '@/components/movie-view/MovieView';
 import Skeleton from '@/components/skeleton/Skeleton';
 import { useFetch } from '@/hooks/useFetch'
 import React from 'react'
+import HomeSwiper from '@/components/swiper/Swiper';
 
 const Home = () => {
   const {data,error,loading} = useFetch("/discover/movie")
 
   return (
     <div>
+      <HomeSwiper movies={data?.results}/>
       <MovieView movies={data?.results}/>
       {loading && <Skeleton count={20}/>}
     </div>
